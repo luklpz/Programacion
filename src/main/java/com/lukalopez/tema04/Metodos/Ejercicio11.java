5e4d4df30bf83529db401c724b49cdb55f0a00b9
luklpz <luka.lopez.j@gmail.com>
2024-10-24 19:00:49 +0200

    Ejercicio11 metodos actualizado.

--- src/main/java/com/lukalopez/tema04/Metodos/Ejercicio11.java
+++ src/main/java/com/lukalopez/tema04/Metodos/Ejercicio11.java
@@ -2,14 +2,14 @@ package com.lukalopez.tema04.Metodos;
 
 public class Ejercicio11 {
     private static int randomInt(int min, int max){
-        int resultado=(int) (Math.random() *max)+min;
+        int resultado=(int) (Math.random() *(max+1))+min;
         return resultado;
     }
 
     private static String quiniela(String mensaje){
         String resultado=mensaje+"\n";
         for (int i=1;i<16;i++){
-            int x = randomInt(0,3);
+            int x = randomInt(0,2);
             if (x==0){
                 resultado+="Partido"+i+"    X"+"\n";
             }
