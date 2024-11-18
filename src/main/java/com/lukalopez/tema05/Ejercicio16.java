74689fd5a464641d6b5cbaf8b4f9d1fb7383b128
luklpz <luka.lopez.j@gmail.com>
2024-11-18 19:24:31 +0100

    Ejercicios 16-17 del tema 5 acabados.

--- src/main/java/com/lukalopez/tema05/Ejercicio16.java
+++ src/main/java/com/lukalopez/tema05/Ejercicio16.java
@@ -7,25 +7,34 @@ import com.lukalopez.lib.Texto;
 
 public class Ejercicio16 {
 
-    private static char letraSegunDNI(int dni){
+    /**
+     * Obtener la letra de un DNI.
+     * @param dni DNI sobre el que se desea obtener su letra.
+     * @return Devuelve un 'char' que contiene la letra del DNI.
+     */
+    public static char letraSegunDNI(int dni){
         String s="TRWAGMYFPDXBNJZSQVHLCKE";
         return s.charAt(dni % 23);
+    }
+
+    private static int solicitarDNI(String mensajeSolicitud){
+        System.out.println(mensajeSolicitud);
+        boolean validado;
+        String texto;
+        do {
+            texto = IO.retirarCaracteresNoNumericos(Escaner.lector.nextLine().trim().replaceAll("\\s+"," "));
+            validado = Texto.validarLongitud(texto, 8, 8, "El DNI ha de tener 8 digitos sin letras. Pruebe otra vez: ");
+        } while (!validado);
+        return Integer.parseInt(texto);
     }
 
     public static void main(String args[]) {
         //Declaramos las variables
         int dni;
-        String texto;
-        boolean validado;
         StringBuilder nif = new StringBuilder();
 
         //Solicitamos el DNI
-        System.out.println("\nIntroduzca a continuación su DNI y le devolveré su NIF: ");
-        do {
-            texto = IO.retirarCaracteresNoNumericos(Escaner.lector.nextLine().trim().replaceAll("\\s+"," "));
-            validado = Texto.validarLongitud(texto, 8, 8, "El DNI ha de tener 8 digitos sin letras. Pruebe otra vez: ");
-        } while (!validado);
-        dni = Integer.parseInt(texto);
+        dni = solicitarDNI("\nIntroduzca a continuación su DNI y le devolveré su NIF: ");
         nif.append(dni);
         //Calculamos la letra segun el DNI y cremos el NIF
         nif.append(letraSegunDNI(dni));
--- src/main/java/com/lukalopez/tema05/Ejercicio17.java
+++ src/main/java/com/lukalopez/tema05/Ejercicio17.java
@@ -1,4 +1,56 @@
 package com.lukalopez.tema05;
 
+import com.lukalopez.lib.Escaner;
+import com.lukalopez.lib.IO;
+
 public class Ejercicio17 {
+
+    /**
+     * Valida si la letra de un NIF es la correcta.
+     *
+     * @param nif Es el NIF que se recibe para realizar la comprobación.
+     * @return Devuelve 'true' si la letra del NIF es la correcta.
+     */
+    private static boolean comprobarNIF(String nif){
+        char letra = nif.charAt(nif.length()-1);
+        int dni = Integer.parseInt(IO.retirarCaracteresNoNumericos(nif));
+        if (letra==Ejercicio16.letraSegunDNI(dni)){
+            return true;
+        }
+        return false;
+    }
+
+    private static String solicitarNif(String mensajeSolicitud){
+        //Definimos variables
+        StringBuilder nif = new StringBuilder();
+        boolean validado= false;
+
+        //Solicitamos y validamos el DNI.
+        System.out.println(mensajeSolicitud);
+        do {
+            nif.append(Escaner.lector.nextLine().replaceAll("\\s+",""));
+            if (nif.isEmpty()){
+                System.err.println("ERROR: No se han introducido valores.");
+            } else if (nif.length()!=9) {
+                System.err.println("ERROR: El NIF ha de tener 9 caracteres");
+            } else if (IO.retirarCaracteresNoNumericos(nif.substring(0,nif.length()-1)).length()!=8) {
+                System.err.println("ERROR: El NIF ha de contener 8 números y una letra al final.");
+            } else {
+                validado=true;
+            }
+            if (!validado){
+                System.out.println("Intentelo de nuevo: ");
+                nif.delete(0,nif.length());
+            }
+        } while (!validado);
+        return nif.toString();
+    }
+
+    public static void main(String args[]) {
+        if (comprobarNIF(solicitarNif("Introduzca a continuación un NIF y validaré si la letra es correcta."))) {
+            System.out.println("La letra del NIF es correcta.");
+        } else {
+            System.out.println("La letra del NIF no es correcta.");
+        }
+    }
 }
