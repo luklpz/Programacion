d8bdcc8f4bac0c2a6b81bc0128e1a41e3f2d61d8
luklpz <luka.lopez.j@gmail.com>
2024-11-20 16:02:19 +0100

    Ejercicio5 de recursividad acabado.

--- src/main/java/com/lukalopez/tema04/Metodos/Recursividad/Ejercicio5.java
+++ src/main/java/com/lukalopez/tema04/Metodos/Recursividad/Ejercicio5.java
@@ -10,19 +10,19 @@ public class Ejercicio5 {
      */
     private static int secuenciaFibonacci(int contador){
         if (contador==0){
-            return -1;
+            return 0;
         }
-        return secuenciaFibonacci(0,1,contador-3);
+        return secuenciaFibonacci(0,1,contador-2);
     }
 
     private static int secuenciaFibonacci(int num1, int num2, int contador){
         if (contador==0){
             return num2;
         }
-        return num2+secuenciaFibonacci(num2,num2+num1,contador-1);
+        return secuenciaFibonacci(num2,num2+num1,contador-1);
     }
 
     public static void main(String args[]) {
-        System.out.println(secuenciaFibonacci(7));
+        System.out.println(secuenciaFibonacci(10));
     }
 }
