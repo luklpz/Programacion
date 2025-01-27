5b0193d7c039caa50602a48b3d0b053c34c0bbaf
luklpz <luka.lopez.j@gmail.com>
2025-01-27 19:25:46 +0100

    EjemplosClase.java creado

---
+++ src/main/java/com/lukalopez/tema06/EjemplosClase.java
@@ -0,0 +1,49 @@
+package com.lukalopez.tema06;
+
+public class EjemplosClase {
+    public static class Jugador {
+        private String nombre;
+        private float portero;
+        private float defensa;
+        private float pase;
+        private float regate;
+        private float tiro;
+        private float estadoForma;
+
+        public Jugador(String nombre, float portero, float defensa, float pase, float regate, float tiro, float estadoForma) {
+            this.nombre = nombre;
+            this.portero = portero;
+            this.defensa = defensa;
+            this.pase = pase;
+            this.regate = regate;
+            this.tiro = tiro;
+            this.estadoForma = estadoForma;
+        }
+
+        public Jugador(String nombre){
+            this(nombre, 50,50,50,50,50,50);
+        }
+
+        public static void entrenar(Jugador idJugador) {
+            idJugador.estadoForma++;
+            idJugador.tiro++;
+        }
+
+        public static void regatear(Jugador idJugador) {
+            idJugador.regate++;
+        }
+
+        public static void chutar(Jugador idJugador) {
+            idJugador.tiro++;
+        }
+    }
+    Jugador futbolista1 = new Jugador("nombre", 32,82,87,80,82,95);
+    Jugador futbolista2 = new Jugador("nombre");
+
+    public void main(String[] args) {
+        System.out.println(futbolista1.estadoForma);
+        Jugador.entrenar(futbolista1);
+        System.out.println("Entrena:");
+        System.out.println(futbolista1.estadoForma);
+    }
+}
