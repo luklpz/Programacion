5b132a3e4168c2339808b6ba60fd876e926a6397
luklpz <luka.lopez.j@gmail.com>
2025-01-27 19:35:35 +0100

    Ejercicio 2, creado objeto e impreso.

--- src/main/java/com/lukalopez/tema06/Ejemplo2.java
+++ src/main/java/com/lukalopez/tema06/Ejemplo2.java
@@ -17,4 +17,9 @@ public class Ejemplo2 {
 
     }
 
+    static Asignatura A1  = new Asignatura("Programacion","1017", Curso.PRIMERO);
+
+    public static void main(String[] args) {
+        System.out.printf("Asignatura: %s \nCodigo: %s \nCurso: %s",A1.nombre,A1.codigo,A1.curso);
+    }
 }
