67d3cb620199afab4be35c599ef2e766bdf823bc
luklpz <luka.lopez.j@gmail.com>
2025-02-04 10:37:32 +0100

    Pila.java, acabada.

--- src/main/java/com/lukalopez/tema06/EstructurasDinamicas/Pila.java
+++ src/main/java/com/lukalopez/tema06/EstructurasDinamicas/Pila.java
@@ -1,4 +1,97 @@
 package com.lukalopez.tema06.EstructurasDinamicas;
 
 public class Pila {
+    //Declaración de las variables.
+    private static final double ERROR = Double.NEGATIVE_INFINITY;
+    private final static int CAPACIDAD_INICIAL = 10;
+    private double[] data;
+    private int size;
+
+    /**
+     * Constructor para una pila vacía con una capacidad inicial de 10 datos.
+     */
+    public Pila(){
+        this(CAPACIDAD_INICIAL);
+    }
+
+    /**
+     * Constructor para una pila vacía.
+     * @param capacidad Cantidad de datos que soporta la pila inicialmente.
+     */
+    public Pila(int capacidad){
+        data = new double[capacidad];
+        size = 0;
+    }
+
+    /**
+     * Añadir un dato a la pila.
+     * @param valor Valor a añadir a la pila.
+     * @return Devuelve 'true' si el proceso se ha completado.
+     */
+    public boolean push(double valor){
+        if (isFull()){
+            expandir();
+        }
+        data[size]=valor;
+        size++;
+        return true;
+    }
+
+    /**
+     * Retira el elemento superior de la pila y lo lee.
+     * @return Devuelve el elemento retirado de la pila.
+     */
+    public double pop(){
+        if (isEmpty()){
+            return ERROR;
+        }
+        size--;
+        return data[size];
+    }
+
+    /**
+     * Lee el número de elementos que contiene la pila.
+     * @return Devuelve el número de elementos que contiene la pila.
+     */
+    public int size(){
+        return size;
+    }
+
+    /**
+     * Lee el último elemento de la pila sin retirarlo.
+     * @return Devuelve el último elemento de la pila.
+     */
+    public double top(){
+        if (isEmpty()){
+            return ERROR;
+        }
+        return data[size];
+    }
+
+    /**
+     * Comprobación de si la pila no tiene datos.
+     * @return Devuelve 'true' si la pila se encuentra vacía.
+     */
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    /**
+     * Comprobación de si la pila ha llegado a su tope.
+     * @return Devuelve true si la pila no acepta más datos.
+     */
+    public boolean isFull(){
+        return size == data.length;
+    }
+
+    /**
+     * Duplicar la capacidad de la pila.
+     * @return Devuelve 'true' si se ha completado el proceso.
+     */
+    public boolean expandir(){
+        double[] arrayCopia = new double[data.length*2];
+        System.arraycopy(data, 0, arrayCopia, 0, size);
+        data=arrayCopia;
+        return true;
+    }
 }
