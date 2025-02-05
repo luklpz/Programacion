6294aa25a0bdbf73b48d46ed3cbd575aaca78deb
luklpz <luka.lopez.j@gmail.com>
2025-02-05 16:55:00 +0100

    Ejercicio2.java, salida modificada por comodidad.

--- src/main/java/com/lukalopez/tema06/EstructurasDinamicas/Ejercicio2.java
+++ src/main/java/com/lukalopez/tema06/EstructurasDinamicas/Ejercicio2.java
@@ -8,7 +8,7 @@ public class Ejercicio2 {
         Pila miPila = new Pila();
 
         //Rellenamos la pila
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             miPila.push(Random.randomDouble(0,20));
             if (i % 5 == 0){
                 System.out.println(miPila.top());
@@ -16,12 +16,12 @@ public class Ejercicio2 {
             System.out.println(miPila.toString());
         }
 
-        for (int i = 19; i > -1; i--) {
+        for (int i = 9; i > -1; i--) {
             miPila.pop();
             if (i % 5 == 0){
                 System.out.println(miPila.top());
             }
-            System.out.println(miPila.toString());
+            System.out.println(miPila);
         }
     }
 }
