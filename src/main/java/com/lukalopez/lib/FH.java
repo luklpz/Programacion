640770c182073eecdb634f7de816164890e17368
luklpz <luka.lopez.j@gmail.com>
2025-03-08 14:41:15 +0100

    Ahora la solicitud de fecha verifica los datos uno por uno

--- src/main/java/com/lukalopez/lib/FH.java
+++ src/main/java/com/lukalopez/lib/FH.java
@@ -12,40 +12,40 @@ public class FH {
     /**
      * Solicita una fecha escrita en el formato indicado.
      *
-     * @param mensaje Mensaje que se imprime para solicitar la fecha.
-     * @param formato Formato en que se desea devolver la fecha.
+     * @param mensajeIncial Mensaje que se imprime antes de solicitar la fecha.
      * @return Es el 'LocalDate' que contiene la fecha en el formato indicado.
      */
-    public static LocalDate solicitarFecha(String mensaje,String formato) {
-        System.out.print(mensaje);
-        String fechaString = Escaner.lector.nextLine();
-        return LocalDate.parse(fechaString, DateTimeFormatter.ofPattern(formato));
+    public static LocalDate solicitarFecha(String mensajeIncial) {
+        System.out.print(mensajeIncial);
+        String fecha = String.format("%02d/%02d/%02d",solicitarDia(), solicitarMes(), solicitarAnyo());
+        System.out.println();
+        return LocalDate.parse(fecha, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
     }
 
     /**
      * Solicita una hora escrita en el formato indicado.
      *
-     * @param mensaje Mensaje que se imprime para solicitar la hora.
-     * @param formato Formato en que se desea devolver la fecha.
+     * @param mensajeIncial Mensaje que se imprime antes de solicitar la hora.
      * @return Es el 'LocalTime' que contiene la hora en el formato indicado.
      */
-    public static LocalTime solicitarHora(String mensaje, String formato) {
-        System.out.print(mensaje);
-        String horaString = Escaner.lector.nextLine();
-        return LocalTime.parse(horaString, DateTimeFormatter.ofPattern(formato));
+    public static LocalTime solicitarHora(String mensajeIncial) {
+        System.out.print(mensajeIncial);
+        String fecha = String.format("%02d:%02d:%02d",solicitarSegundos(), solicitarMinutos(), solicitarHoras());
+        System.out.println();
+        return LocalTime.parse(fecha, DateTimeFormatter.ofPattern("ss:mm:hh"));
     }
 
     /**
      * Solicita una fecha y una hora escritas en el formato indicado.
      *
-     * @param mensaje Mensaje que se imprime para solicitar la fecha y la hora.
-     * @param formato Formato en que se desea devolver la fecha.
+     * @param mensajeIncial Mensaje que se imprime para solicitar la fecha y la hora.
      * @return Es el 'LocalDateTime' que contiene la fecha y la hora en el formato indicado.
      */
-    public static LocalDateTime solicitarFechaHora(String mensaje, String formato) {
-        System.out.print(mensaje);
-        String fechaHoraString = Escaner.lector.nextLine();
-        return LocalDateTime.parse(fechaHoraString, DateTimeFormatter.ofPattern(formato));
+    public static LocalDateTime solicitarFechaHora(String mensajeIncial) {
+        System.out.print(mensajeIncial);
+        String fecha = String.format("%02d/%02d/%02d %02d:%02d:%02d",solicitarDia(),solicitarMes(),solicitarAnyo(),solicitarSegundos(), solicitarMinutos(), solicitarHoras());
+        System.out.println();
+        return LocalDateTime.parse(fecha, DateTimeFormatter.ofPattern("ss:mm:hh"));
     }
 
 
@@ -85,5 +85,57 @@ public class FH {
     public static String formatearFechaHora(LocalDateTime fechaHora,String formato){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(formato);
         return fechaHora.format(formatter);
+    }
+
+
+
+//******************************** SOLICITUD DATOS ********************************\\
+
+    /**
+     * Mét0do para solicitar segundos.
+     * @return Número de segundos validados.
+     */
+    private static int solicitarSegundos(){
+        return IO.solicitarInt("Introduzca los segundos: ",0,60,Error.toString(Error.FUERA_DE_RANGO));
+    }
+
+    /**
+     * Mét0do para solicitar minutos.
+     * @return Número de minutos validados.
+     */
+    private static int solicitarMinutos(){
+        return IO.solicitarInt("Introduzca los minutos: ",0,60,Error.toString(Error.FUERA_DE_RANGO));
+    }
+
+    /**
+     * Mét0do para solicitar horas.
+     * @return Número de horas validados.
+     */
+    private static int solicitarHoras(){
+        return IO.solicitarInt("Introduzca las horas: ",0,60,Error.toString(Error.FUERA_DE_RANGO));
+    }
+
+    /**
+     * Mét0do para solicitar dias.
+     * @return Número de dias validados.
+     */
+    private static int solicitarDia(){
+        return IO.solicitarInt("Introduzca el dia: ",0,60,Error.toString(Error.FUERA_DE_RANGO));
+    }
+
+    /**
+     * Mét0do para solicitar meses.
+     * @return Número de meses validados.
+     */
+    private static int solicitarMes(){
+        return IO.solicitarInt("Introduzca el mes: ",1,12,Error.toString(Error.FUERA_DE_RANGO));
+    }
+
+    /**
+     * Mét0do para solicitar un año.
+     * @return Numero del año validado.
+     */
+    private static int solicitarAnyo(){
+        return IO.solicitarInt("Introduzca el año: ",0,60,Error.toString(Error.FUERA_DE_RANGO));
     }
 }
